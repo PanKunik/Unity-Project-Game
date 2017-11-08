9b9cd8eb8c1c10624bf56ba5b7c2e9463e6bb945
gazeta12254 <kunik12254@o2.pl>
2017-11-09 00:09:41 +0100

    Added PopUpDamage for multiple objects

--- Assets/Scripts/Enemy/EnemyStats.cs
+++ Assets/Scripts/Enemy/EnemyStats.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class EnemyStats : CharacterStats {
     Slider img;
 
+    public GameObject CBTprefabs;
+
     protected override void Awake()
     {
         base.Awake();
@@ -15,8 +17,22 @@ public class EnemyStats : CharacterStats {
     public override void TakeDamage(int amount)
     {
         base.TakeDamage(amount);
-
+        InitCBT(amount.ToString());
+        
+        //FloatingTextController.CreateFloatingText(amount, transform);
         img.value = currentHealth;
+    }
+
+    void InitCBT(string text)
+    {
+        GameObject temp = Instantiate(CBTprefabs) as GameObject;
+        RectTransform tempRect = temp.GetComponent<RectTransform>();
+        temp.transform.SetParent(transform.Find("EnemyCanvas"));
+        tempRect.transform.localPosition = CBTprefabs.transform.localPosition;
+        tempRect.transform.localScale = CBTprefabs.transform.localScale;
+        tempRect.transform.localRotation = CBTprefabs.transform.localRotation;
+
+        temp.GetComponent<Text>().text = text;
     }
 
     public override void Die()
