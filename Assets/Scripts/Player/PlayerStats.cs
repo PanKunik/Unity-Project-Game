ccedf50764ff4077423df00ecf4b995c346080e2
gazeta12254 <kunik12254@o2.pl>
2017-11-15 18:58:53 +0100

    Dodaję animację śmierci

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -1,11 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerStats : CharacterStats {
+
+    Animator anim;
+    PlayerMovement playerMov;
+    PlayerController playerControl;
+    CharacterCombat Enemy;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        anim = GetComponent<Animator>();
+        
+        playerMov = GetComponent<PlayerMovement>();
+        playerControl = GetComponent<PlayerController>();
+        Enemy = GameObject.FindWithTag("Enemy").GetComponent<CharacterCombat>();
+    }
 
     public override void Die()
     {
         base.Die();
+        anim.SetTrigger("Die");
+        playerMov.enabled = false;
+        playerControl.enabled = false;
+        Enemy.enabled = false;
     }
 }
