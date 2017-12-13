5ab1cbff78fd85fa03d3f5e71037d8c94924947a
gazeta12254 <kunik12254@o2.pl>
2017-12-13 20:41:38 +0100

    Update 0.2
    -Dodaję MENU główne
    -Dodaję animację tła w MENU
    -dodaję muzykę do MENU
    -dodaję ustawienia gry

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine.Audio;
+using System.Collections;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public Sound[] sounds;
+    float vol;
+
+    public static AudioManager instance;
+
+    // Use this for initialization
+    void Awake()
+    {
+        vol = 0f;
+
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+            s.source.outputAudioMixerGroup = s.mixer;
+        }
+    }
+
+    void Start()
+    {
+        Play("MenuBackground");
+        
+    }
+
+    private void Update()
+    {
+        if (vol <= 1)
+        {
+            vol += Time.deltaTime * 0.2f;
+            FadeIn("MenuBackground");
+        }
+
+    }
+
+    public void FadeIn(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+        s.source.volume = vol;
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+        s.source.Play();
+    }
+
+    // FindObjectOfType<AudioManager>().Play("PlayerDeath");
+}
